41871e5afcafb35fc98f5278dc5c3fb8252d6319
McTeaCup <69859445+McTeaCup@users.noreply.github.com>
2020-08-19 15:07:44 +0200

    Player Movement Complete
    The only thing missing is the ability to rotate the player model

--- Rymden/Assets/Scripts/Player/PlayerMovemetn.cs
+++ Rymden/Assets/Scripts/Player/PlayerMovemetn.cs
@@ -4,43 +4,65 @@ using UnityEngine;
 
 public class PlayerMovemetn : MonoBehaviour
 {
+    [Header("Player Properties")]
     public float movementSpeed;
-    public float boostSpeedMultiplier;
-    public float maxVelocity = 20;
+    [Tooltip("Player's top speed")]
+    public float maxVelocity = 20.0f;
+    [Tooltip("Big value = Fast break | Low value = Slow break\nRecommended value: 0.2")]
+    public float slowDown = 0.2f;
 
-    float boosSpeed = 1;
+    float _boosSpeed = 2;
+    Rigidbody2D _playerRB;
 
-    Rigidbody2D playerRB;
-
-    private void Start()
+    private void Awake()
     {
-        playerRB = transform.GetComponent<Rigidbody2D>();
-        playerRB.gravityScale = 0;
+        _playerRB = transform.GetComponent<Rigidbody2D>();
+        _playerRB.gravityScale = 0;                         //Turn of gravity (IN SPACE? WHAT IRON????)
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
-        Debug.Log(playerRB.velocity);
     }
 
     void PlayerMovement()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");      //Declares if the player goes right/left
+        float y = Input.GetAxis("Vertical");        //Declares if the player goes up/down
 
-        playerRB.AddForce(transform.right * ((x * movementSpeed) * boosSpeed));
-        playerRB.AddForce(transform.up * ((y * movementSpeed) * boosSpeed));
+        _playerRB.AddForce(transform.right * ((x * movementSpeed) * _boosSpeed));       //Applies force right/left
+        _playerRB.AddForce(transform.up * ((y * movementSpeed) * _boosSpeed));          //Applies force up/down
 
-        if (Input.GetKey(KeyCode.Space))
+        #region Player Speed
+
+        #region Max Speed Manager
+        //Speed capper
+        if (_playerRB.velocity.x > maxVelocity || _playerRB.velocity.x < (maxVelocity * -1)) //If the player moves too fast to the right/left
         {
-            boosSpeed = boostSpeedMultiplier;
+            _playerRB.AddForce(-transform.right * ((x * movementSpeed) * _boosSpeed));
         }
 
-        if(playerRB.velocity.x > maxVelocity && playerRB.velocity.x < (maxVelocity * -1))
+        if (_playerRB.velocity.y > maxVelocity || _playerRB.velocity.y < (maxVelocity * -1)) //If the player moves too fast to the right/left
         {
-            playerRB.velocity = new Vector2(maxVelocity, playerRB.velocity.y);
+            _playerRB.AddForce(-transform.up * ((y * movementSpeed) * _boosSpeed));
         }
+        #endregion
+
+        #region Speed Breaker
+        //Velocity Break
+        if (Input.GetAxis("Horizontal") == 0)       //If no input slow player down
+        {
+            _playerRB.AddForce(-transform.right * (_playerRB.velocity.x * slowDown));
+        }
+
+        if (Input.GetAxis("Vertical") == 0)         //If no input slow player down
+        {
+            _playerRB.AddForce(-transform.up * (_playerRB.velocity.y * slowDown));
+        }
+
+        #endregion
+
+        #endregion
     }
 }
