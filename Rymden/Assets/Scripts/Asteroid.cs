7be673ec32c221f03540fee30a3e3ae261a21d59
Arbuzee <37773311+Arbuzee@users.noreply.github.com>
2020-08-19 12:20:40 +0200

    Bullet and asteroid

---
+++ Rymden/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private float health;
+    [SerializeField] private float maxHealth = 100;
+
+    [SerializeField] private ParticleSystem hitParticle;
+    [SerializeField] private ParticleSystem deathParticle;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //other.GetComponent<PlayerVariables>().TakeDamage();
+            Debug.Log("Player Hit by asteroid");
+        }
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+
+        OnHit();
+
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void OnHit()
+    {
+        Instantiate(hitParticle, transform.position, Quaternion.identity);
+    }
+
+    private void Death()
+    {
+        Instantiate(deathParticle, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Rymden/Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [SerializeField] private float damage = default;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(new Vector2(0, 500));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Asteroid>().TakeDamage(damage);
+        }
+    }
+}
