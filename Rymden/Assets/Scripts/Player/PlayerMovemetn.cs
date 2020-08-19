582dc4f04be57af772b205a13c5881cfc9bebc47
McTeaCup <69859445+McTeaCup@users.noreply.github.com>
2020-08-19 13:01:34 +0200

    Player Movment
    Försöker fortfarande fixa så att det finns en max cap på hur snabbt spelaren rör sig, men har inte hittat något än

--- Rymden/Assets/Scripts/Player/PlayerMovemetn.cs
+++ Rymden/Assets/Scripts/Player/PlayerMovemetn.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerMovemetn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movementSpeed;
+    public float boostSpeedMultiplier;
+    public float maxVelocity = 20;
+
+    float boosSpeed = 1;
+
+    Rigidbody2D playerRB;
+
+    private void Start()
     {
-        
+        playerRB = transform.GetComponent<Rigidbody2D>();
+        playerRB.gravityScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+        Debug.Log(playerRB.velocity);
+    }
+
+    void PlayerMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        playerRB.AddForce(transform.right * ((x * movementSpeed) * boosSpeed));
+        playerRB.AddForce(transform.up * ((y * movementSpeed) * boosSpeed));
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            boosSpeed = boostSpeedMultiplier;
+        }
+
+        if(playerRB.velocity.x > maxVelocity && playerRB.velocity.x < (maxVelocity * -1))
+        {
+            playerRB.velocity = new Vector2(maxVelocity, playerRB.velocity.y);
+        }
     }
 }
