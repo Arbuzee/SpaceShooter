0bc7bb07ff65e5cf11983e3d9e315917f215b316
McTeaCup <69859445+McTeaCup@users.noreply.github.com>
2020-08-20 15:40:01 +0200

    stuff

---
+++ Rymden/Assets/Scripts/RaritySpawn.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaritySpawn : MonoBehaviour
+{
+    [SerializeField]
+    int _powerupIndex;
+    int _rarity;
+
+    private void Awake()
+    {
+        _powerupIndex = Random.Range(0, 4);
+        _rarity = Random.Range(0, 3);
+    }
+
+    void Decide()
+    {
+        switch (_powerupIndex)  //Choos an ability
+        {
+            #region Ability 1
+            case 0:     
+                {
+                    #region Rarity
+
+                    switch (_rarity)    
+                    {
+                        case 0:     //Common
+                            {
+                                return;
+                            }
+
+                        case 1:     //Rare
+                            {
+                                return;
+                            }
+
+                        case 2:     //Legendery
+                            {
+                                return;
+                            }
+                    }
+
+                    return;
+                    #endregion
+                }
+                #endregion
+
+            #region Ability 1
+            case 1:     
+                {
+                    #region Rarity
+
+                    switch (_rarity)    
+                    {
+                        case 0:     //Common
+                            {
+                                return;
+                            }
+
+                        case 1:     //Rare
+                            {
+                                return;
+                            }
+
+                        case 2:     //Legendery
+                            {
+                                return;
+                            }
+                    }
+
+                    return;
+                    #endregion
+                }
+                #endregion
+        }
+
+        //Choose a rarity
+    }
+}
