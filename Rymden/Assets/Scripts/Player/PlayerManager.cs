cdf9b5d4c838e023d372dfad91b3a0158c8e3dd0
McTeaCup <69859445+McTeaCup@users.noreply.github.com>
2020-08-20 10:53:44 +0200

    Update to movement + more
    -Player outline now swiches colors dependong on health
    -Player prefab created
    -Player turning added (not rotating 100% correct)
    -General fixes for player movement

---
+++ Rymden/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [Header("Player Properties")]
+    public float playerStartHealth = 100;
+
+    [Header("Cosmetics")]
+    public Gradient playerHealthColor;
+
+    [SerializeField]
+    float _playerCurrentHealth;
+
+    GameObject _player;
+    MeshRenderer _playerMaterial;
+
+    private void Awake()
+    {
+        _player = transform.GetChild(1).gameObject;
+        _playerMaterial = _player.GetComponent<MeshRenderer>();
+        _playerCurrentHealth = playerStartHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _healthFloat = _playerCurrentHealth / playerStartHealth;
+        _playerMaterial.sharedMaterial.SetColor("_EmissionColor", playerHealthColor.Evaluate(_healthFloat) * 5);
+    }
+}
--- Rymden/Assets/Scripts/Player/PlayerMovemetn.cs
+++ Rymden/Assets/Scripts/Player/PlayerMovemetn.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovemetn : MonoBehaviour
 {
@@ -10,43 +8,56 @@ public class PlayerMovemetn : MonoBehaviour
     public float maxVelocity = 20.0f;
     [Tooltip("Big value = Fast break | Low value = Slow break\nRecommended value: 0.2")]
     public float slowDown = 0.2f;
+    public float sprintMultiplier = 2;
+    [HideInInspector]
+    public bool playerIsBoosting;
 
-    float _boosSpeed = 2;
+    //Mouse variables
+    GameObject _playerLookpoint;
+    GameObject _playerModel;
+    Camera _playerCamera;
+
     Rigidbody2D _playerRB;
 
     private void Awake()
     {
         _playerRB = transform.GetComponent<Rigidbody2D>();
         _playerRB.gravityScale = 0;                         //Turn of gravity (IN SPACE? WHAT IRON????)
+        _playerLookpoint = transform.GetChild(0).gameObject;
+        _playerModel = transform.GetChild(1).gameObject;
+        _playerCamera = transform.GetChild(2).GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
+        Debug.Log(_playerRB.velocity);
+        PlayerLook();
     }
 
     void PlayerMovement()
     {
-        float x = Input.GetAxis("Horizontal");      //Declares if the player goes right/left
-        float y = Input.GetAxis("Vertical");        //Declares if the player goes up/down
+        float _x = Input.GetAxis("Horizontal");      //Declares if the player goes right/left
+        float _y = Input.GetAxis("Vertical");        //Declares if the player goes up/down
 
-        _playerRB.AddForce(transform.right * ((x * movementSpeed) * _boosSpeed));       //Applies force right/left
-        _playerRB.AddForce(transform.up * ((y * movementSpeed) * _boosSpeed));          //Applies force up/down
+        _playerRB.AddForce(transform.right * ((_x * movementSpeed) * 2));       //Applies force right/left
+        _playerRB.AddForce(transform.up * ((_y * movementSpeed) * 2));          //Applies force up/down
 
         #region Player Speed
 
         #region Max Speed Manager
         //Speed capper
-        if (_playerRB.velocity.x > maxVelocity || _playerRB.velocity.x < (maxVelocity * -1)) //If the player moves too fast to the right/left
+        if (_playerRB.velocity.x > maxVelocity && _x != -1 || _playerRB.velocity.x < (maxVelocity * -1) && _x != 1) //If the player moves too fast to the right/left
         {
-            _playerRB.AddForce(-transform.right * ((x * movementSpeed) * _boosSpeed));
+            _playerRB.AddForce(-transform.right * ((_x * movementSpeed) * 2));
         }
 
-        if (_playerRB.velocity.y > maxVelocity || _playerRB.velocity.y < (maxVelocity * -1)) //If the player moves too fast to the right/left
+        if (_playerRB.velocity.y > maxVelocity && _y != -1 || _playerRB.velocity.y < (maxVelocity * -1) && _y != 1) //If the player moves too fast to the right/left
         {
-            _playerRB.AddForce(-transform.up * ((y * movementSpeed) * _boosSpeed));
+            _playerRB.AddForce(-transform.up * ((_y * movementSpeed) * 2));
         }
+
         #endregion
 
         #region Speed Breaker
@@ -64,5 +75,16 @@ public class PlayerMovemetn : MonoBehaviour
         #endregion
 
         #endregion
+    }
+
+    void PlayerLook()
+    {
+        Vector3 worldPos;
+        Vector3 mousePos = Input.mousePosition;                     //Gets mouse pos
+        worldPos = _playerCamera.ScreenToWorldPoint(mousePos);      //Converts mouse pos to world pos
+
+        _playerLookpoint.transform.position = new Vector3(worldPos.x, worldPos.y, transform.position.z);
+
+        _playerModel.transform.LookAt(_playerLookpoint.transform.position);     //Turns player to mouse
     }
 }
